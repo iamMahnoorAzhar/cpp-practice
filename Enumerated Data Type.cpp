c5c1f43ad09e9aa21bc5c22694022a7a312b8b03
iamMahnoorAzhar <mahnoorazhar2004@gmail.com>
2024-12-30 20:27:44 +0500

    Add files via upload

---
+++ Enumerated Data Type.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+enum DoW
+{
+	Mon = 1,
+	Tue,
+	Wed,
+	Thu,
+	Friday,
+	Saturday,
+	Sunday
+};
+
+enum Religion
+{
+	Muslim,
+	NonMuslim
+};
+int main()
+{
+	
+	
+}
